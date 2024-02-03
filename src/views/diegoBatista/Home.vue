ac87173f2871bbf30db5aca0427d78559ebfd563
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2024-02-03 13:11:16 -0300

    estilizado tela principal home

--- src/views/diegoBatista/Home.vue
+++ src/views/diegoBatista/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="main-content">
     <div>
-      <h1>Me Leva pra Casa </h1>
-      <p>Nós cuidamos deles. Agora deixe que eles cuidem de você. </p>
-      <p>Aqui a gente cuida de animais resgatados de áreas evacuadas de Minas Gerais e queremos compartilhar todo esse
+      <h1 class="title">Me Leva pra Casa </h1>
+      <p class="text-description">Nós cuidamos deles. Agora deixe que eles cuidem de você. </p>
+      <p class="text-description">Aqui a gente cuida de animais resgatados de áreas evacuadas de Minas Gerais e queremos compartilhar todo esse
         amor com mais pessoas por meio da adoção.</p>
-      <p>Ter um animal de estimação traz benefícios para a saúde física e mental. É uma troca de amor e carinho que faz
+      <p class="text-description">Ter um animal de estimação traz benefícios para a saúde física e mental. É uma troca de amor e carinho que faz
         bem pra todo mundo. Leve um para casa: cuide dele e deixe que ele cuide de você também. </p>
         <router-link to="/pets-adocao">
-          <button>Adote Agora</button>
+          <button class="button-action">Adote Agora</button>
         </router-link>        
     </div>
     <div>
@@ -18,4 +18,45 @@
 </template>
 
 
-<style scoped></style>
+<style scoped>
+.title {
+  font-size: 44px;
+  color: rgb(225, 73, 18);
+}
+
+.text-description {
+  font-size: 20px;
+  text-align: justify;
+}
+
+.main-content {
+  display: flex;
+  width: 70%;
+  margin: 0 auto;
+  align-items: center;
+}
+
+.main-image {
+  width: 400px;
+  object-fit: contain;
+}
+
+.button-action {
+    width: 150px;
+    height: 44px;
+    background: rgb(225, 73, 18);
+    border-radius: 4px;
+    color: #fff;
+    font-size: 18px;
+}
+
+@media (max-width: 1000px) {
+  .main-content {
+    flex-direction: column-reverse;
+  }
+
+  .text-description {
+    font-size: 20px;
+  }
+}
+</style>
