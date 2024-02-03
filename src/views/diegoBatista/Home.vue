ddef6347ee28e92e3abe2aed043041ef232ffc7a
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2024-02-02 21:49:21 -0300

    Criado telas home, pets e profile para inicializar o desenvolvimento do projeto

--- src/views/diegoBatista/Home.vue
+++ src/views/diegoBatista/Home.vue
@@ -1,3 +1,21 @@
 <template>
-  <span>  código pessoa 1</span>
+  <div class="main-content">
+    <div>
+      <h1>Me Leva pra Casa </h1>
+      <p>Nós cuidamos deles. Agora deixe que eles cuidem de você. </p>
+      <p>Aqui a gente cuida de animais resgatados de áreas evacuadas de Minas Gerais e queremos compartilhar todo esse
+        amor com mais pessoas por meio da adoção.</p>
+      <p>Ter um animal de estimação traz benefícios para a saúde física e mental. É uma troca de amor e carinho que faz
+        bem pra todo mundo. Leve um para casa: cuide dele e deixe que ele cuide de você também. </p>
+        <router-link to="/pets-adocao">
+          <button>Adote Agora</button>
+        </router-link>        
+    </div>
+    <div>
+      <img src="https://vale.com/documents/d/guest/imagem-dogs01" alt="logo-principal" class="main-img">
+    </div>
+  </div>
 </template>
+
+
+<style scoped></style>
