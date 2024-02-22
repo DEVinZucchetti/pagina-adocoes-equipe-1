3f0057e4d9f21a33274554aabe89a1b645d5505e
Diego B Morais <128264029+diegobmorais@users.noreply.github.com>
2024-02-22 14:45:57 -0300

    corrigido erro com os teste no pets.spec, alterado nome do pet e rota para mocar a rota de adoção por id

--- src/views/diegoBatista/pets.spec.js
+++ src/views/diegoBatista/pets.spec.js
@@ -1,52 +1,59 @@
 import { flushPromises, mount } from "@vue/test-utils";
-import { describe, expect, it, vi } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import Pets from "./Pets.vue";
 import PetService from "@/services/PetService";
 
 
-describe ('tela de listagem de pets', () => {
+describe('tela de listagem de pets', () => {
 
-    vi.spyOn(PetService, 'getAllPets').mockResolvedValue([
+    beforeEach(() => {
+        vi.spyOn(PetService, 'getAllPets').mockResolvedValue([
 
-     {  id: 1,
-        name: "Toto",
-        age: 4}
-    ])
-
-    it("espera-se que a tela seja renderizada", ()=>{
-
-        const component = mount(Pets)
-        
-        expect(component).toBeTruthy()
+            {
+                id: 2,
+                pet_name: "Magu",
+                age: 3
+            }
+        ])
     })
-    
-    it("espera-se que liste um card para cada pet", async ()=>{
 
-        const component = mount(Pets)
+
+    it("espera-se que a tela seja renderizada", async () => {
+
+        const wrapper = mount(Pets)
+
         await flushPromises()
 
-        expect(component.text()).toContain("Toto")
+        expect(wrapper).toBeTruthy()
+    })
+
+    it("espera-se que liste um card para cada pet", async () => {
+
+        const wrapper = mount(Pets)
+        await flushPromises()
+        
+        expect(wrapper.text()).toContain("Magu")
 
     })
 
-    it("espera-se que navegue para pagina de perfil do pet clicado", async ()=>{
+    it("espera-se que navegue para pagina de perfil do pet clicado", async () => {
 
         const mockRouter = {
             push: vi.fn()
         }
 
-        const component = mount(Pets, {
-            global :{
-                mocks:{
+        const wrapper = mount(Pets, {
+            global: {
+                mocks: {
                     $router: mockRouter
                 }
             }
-        })        
-       
-        await flushPromises()
-        
-        component.findAll("[data-test='item-pet']")[0].trigger("click")
+        })
 
-        expect(mockRouter.push).toHaveBeenCalledWith('http://localhost:5173/pets-adocao/1/perfil')
+        await flushPromises()
+
+        wrapper.findAll("[data-test='item-pet']")[0].trigger("click")
+
+        expect(mockRouter.push).toHaveBeenCalledWith('/pets-adocao/2/perfil')
     })
 })
